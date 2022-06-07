d177206502e1ad6432eb300e22d72b012124bbc1
Aurelio <aurelio.machado94@gmail.com>
2022-06-07 01:05:21 -0300

    Projeto laravel

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Aurelio Costa',
+            'email' => 'aurelio@email.com.br',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+}
+ /*
+            User::create([
+            'name' => 'Carlos Ferreira',
+            'email' => 'carlos@especializati.com.br',
+            'password' => bcrypt('12345678'),
+        ]);
+ */
